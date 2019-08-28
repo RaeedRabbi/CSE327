827cfad9f839c01c6d03a7ec6997f562abdeb9fb
Raeed Rabbi <52441249+RaeedRabbi@users.noreply.github.com>
2019-08-28 17:54:33 +0600

    kernal is updated for admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user' =>  \App\Http\Middleware\UserMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class
 
     ];
 
