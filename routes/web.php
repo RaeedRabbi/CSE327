1a808a33384ad11bd73c79320e108c8ec5565510
Raeed Rabbi <52441249+RaeedRabbi@users.noreply.github.com>
2019-08-29 16:23:41 +0600

    commented on web.php

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//page routes
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 Route::get('/blog', 'PagesController@blog');
 
+//shop routes
 Route::get('/shop', 'ShopController@index');
 Route::get('/shop/{product}', 'ShopController@show');
 
-//Route::resource('wishList', 'WishListController')->only('index');
+//dashboard
 Auth::routes();
 Route::get('/dashboard', 'DashboardController@index');
 
 
-
+//cart routes
 Route::get('/cart', 'CartController@index')->name('cart.index');
 Route::post('/cart/{id}/add', 'CartController@store')->name('cart.add');
 Route::delete('/cart/{product}/delete', 'CartController@destroy')->name('cart.delete');
 Route::post('/cart/{product}/update', 'CartController@update')->name('cart.update');
 Route::delete('/cart', 'CartController@clearCart')->name('cart.clear');
 
+//wishlist routes
 Route::middleware(['auth'])->group(function(){
     Route::post('/shop/{product}/review', 'ShopController@storeReview');
     Route::get('/wishlist', 'WishListController@index');
@@ -38,6 +40,8 @@ Route::middleware(['auth'])->group(function(){
     Route::delete('/wishlist/{product}/delete', 'WishListController@destroy');
 
 });
+
+//admin routes
 Route::prefix('admin')->group(function () {
     Route::resource('productmanager', 'ProductController')->middleware('admin');
 });
